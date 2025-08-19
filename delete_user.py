5be521e4fcc467222470c1c6c362a842a07f3a04
davizji <102144262+davizji@users.noreply.github.com>
2025-08-19 12:42:19 +0200

    Update delete_user.py

--- delete_user.py
+++ delete_user.py
@@ -1,21 +1,21 @@
 from pyzabbix import ZabbixAPI
 
-zabbix_url = 'https://eventmgt11.damovo.de/api_jsonrpc.php'
-zabbix_user = 'dawid.wrobel@damovo.com'
-zabbix_password = 'Zabbix123?'
+zabbix_url = 'zabbix.url/api_jsonrpc.php'
+zabbix_user = 'username'
+zabbix_password = 'password'
 
-# Logowanie
+# Logging
 zapi = ZabbixAPI(zabbix_url)
 zapi.login(zabbix_user, zabbix_password)
 
-# Pobierz ID użytkownika decare1
-users = zapi.user.get(filter={'username': 'decare1'})
+# Get user ID login_name
+users = zapi.user.get(filter={'username': 'login_name'})
 if not users:
-    print("Użytkownik 'decare1' nie istnieje lub już został usunięty.")
+    print("User 'login_name' not existing or already removed.")
 else:
-    decare1_id = users[0]['userid']
-    # Usuń użytkownika
-    zapi.user.delete(decare1_id)
-    print("Użytkownik 'decare1' został usunięty.")
+    login_name_id = users[0]['userid']
+    # Remove user
+    zapi.user.delete(login_name_id)
+    print("User 'login_name' removed.")
 
-print("Gotowe.")
+print("Ready.")
