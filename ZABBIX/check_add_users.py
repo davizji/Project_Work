1ea1fb210b3fa66026dee11556e48927b99802f1
davizji <102144262+davizji@users.noreply.github.com>
2025-08-19 16:15:56 +0200

    Update check_add_users.py

--- ZABBIX/check_add_users.py
+++ ZABBIX/check_add_users.py
@@ -7,10 +7,15 @@ zabbix_password = 'zabbix_password'
 zapi = ZabbixAPI(zabbix_url)
 zapi.login(zabbix_user, zabbix_password)
 
-usernames_to_add = [    
-    {'alias': 'test3', 'passwd': 'testtest123', 'usrgrps': [{"usrgrpid": "8"}]},
-    {'alias': 'test4', 'passwd': 'testtest124', 'usrgrps': [{"usrgrpid": "8"}]},
-    {'alias': 'test10', 'passwd': 'testtest125', 'usrgrps': [{"usrgrpid": "8"}]}]
+user_role = zapi.role.get()
+print(user_role)
+
+usernames_to_add = [
+    {'alias': 'test3', 'passwd': 'testtest123', 'usrgrps': [{"usrgrpid": "8"}], 'roleids': "4"},
+    {'alias': 'test4', 'passwd': 'testtest124', 'usrgrps': [{"usrgrpid": "8"}], 'roleids': "4"},
+    {'alias': 'test10', 'passwd': 'testtest125', 'usrgrps': [{"usrgrpid": "8"}], 'roleids': "4"},
+    {'alias': 'test11', 'passwd': 'testtest125', 'usrgrps': [{"usrgrpid": "8"}], 'roleids': "4"} 
+    ]
 
 for usernames in usernames_to_add:
     users = zapi.user.get(filter={'username':usernames['alias']})
@@ -20,6 +25,7 @@ for usernames in usernames_to_add:
         zapi.user.create(
                 username=usernames['alias'],
                 passwd=usernames['passwd'],
-                usrgrps=usernames['usrgrps']
+                usrgrps=usernames['usrgrps'],
+                roleid=usernames['roleids']
             )
         print(f"User {usernames['alias']} created")
