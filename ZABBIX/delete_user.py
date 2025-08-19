50ac9eb38c23c5386769c7f6d51e73c65c33d75e
davizji <102144262+davizji@users.noreply.github.com>
2025-08-19 14:11:53 +0200

    Update delete_user.py

--- ZABBIX/delete_user.py
+++ ZABBIX/delete_user.py
@@ -9,13 +9,14 @@ zapi = ZabbixAPI(zabbix_url)
 zapi.login(zabbix_user, zabbix_password)
 
 # Get user ID login_name
-users = zapi.user.get(filter={'username': 'login_name'})
+users = zapi.user.get(filter={'username': 'test'})
 if not users:
-    print("User 'login_name' not existing or already removed.")
+    print("User 'test' not existing or already removed.")
 else:
-    login_name_id = users[0]['userid']
+    test_id = users[0]['userid']
     # Remove user
-    zapi.user.delete(login_name_id)
-    print("User 'login_name' removed.")
+    zapi.user.delete(test_id)
+    print("User 'test' removed.")
 
 print("Ready.")
+
